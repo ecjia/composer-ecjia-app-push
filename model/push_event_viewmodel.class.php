34971bbb1043285fa564f062cadb3d6853f2dc30
王正东 <hxroyal@gmail.com>
2017-04-24 11:30:38 +0800

    优化数据模型类

--- model/push_event_viewmodel.class.php
+++ model/push_event_viewmodel.class.php
@@ -5,8 +5,8 @@ class push_event_viewmodel extends Component_Model_View {
 	public $table_name = '';
 	public $view = array();
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
+		// $this->db_config = RC_Config::load_config('database');
+		// $this->db_setting = 'default';
 		$this->table_name = 'push_event';
 		$this->table_alias_name = 'pe';
 		
