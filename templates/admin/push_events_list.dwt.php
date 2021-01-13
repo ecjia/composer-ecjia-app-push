48d30c8c8cdf79f3a31cfd0d94bfc21f9f065473
王正东 <wangzhengdong@ecmoban.com>
2021-01-13 10:44:27 +0800

    优化推送事件列表

--- templates/admin/push_events_list.dwt.php
+++ templates/admin/push_events_list.dwt.php
@@ -21,29 +21,40 @@
 
 <div class="row-fluid search_page">
 	<div class="span12">
-         <div class="search_panel clearfix">
-		   <!-- {foreach from=$data item=val} -->
-           	 <div class="search_item clearfix">
-           	  	<div class="pull-right">
-	                <a class="change_status" style="cursor: pointer;"  data-msg='{if $val.status eq 'open'}{t domain="push"}您确定要关闭该消息事件吗？{/t}{else}{t domain="push"}您确定要开启该消息事件吗？{/t}{/if}' data-href='{if $val.status eq "open"}{url path="push/admin_events/close" args="code={$val.code}&id={$val.id}"}{else}{url path="push/admin_events/open" args="code={$val.code}&id={$val.id}"}{/if}' >
-                       {if $val.status eq 'open'}
-                       <button class="btn" type="button" style="margin-top: 20px;">{t domain="push"}点击关闭{/t}</button>  
-                       {else}
-                       <button class="btn btn-gebo" type="button" style="margin-top: 20px;">{t domain="push"}点击开启{/t}</button> 
-                       {/if}
-	                </a>        
-                </div>
-                
-                <div class="search_content" style="padding-left: 0px;">
-                    <h4>
-                        <a class="sepV_a" style="text-decoration:none;">{$val.name}</a>{if $val.status eq 'open'}<span class="label label-info">{t domain="push"}开启中{/t}</span>{else}<span class="label">{t domain="push"}已关闭{/t}</span>{/if}    
-                    </h4>
-                    <p class="sepH_a"><strong>{$val.code}</strong></p>
-                    <p class="sepH_b item_description">{$val.description}</p>
-                </div>
-             </div>
-         <!-- {/foreach} -->  
-        </div>
+        <table class="table table-striped smpl_tbl dataTable table-hide-edit" id="plugin-table">
+            <thead>
+            <tr>
+                <th class="w50">{t domain="push"}编号{/t}</th>
+                <th class="w300">{t domain="push"}事件代号{/t}</th>
+                <th>{t domain="push"}事件名称{/t}</th>
+                <th>{t domain="push"}事件描述{/t}</th>
+                <th>{t domain="push"}事件状态{/t}</th>
+                <th>{t domain="push"}操作{/t}</th>
+            </tr>
+            </thead>
+            <tbody>
+            <!-- {foreach from=$data key=key item=list name=data} -->
+            <tr>
+                <td>{$smarty.foreach.data.index+1}</td>
+                <td>{$list.code}</td>
+                <td>{$list.name}</td>
+                <td>{$list.description}</td>
+                <td>{if $list.status eq 'open'}<span class="label label-info">{t domain="push"}开启中{/t}</span>{else}<span class="label">{t domain="push"}已关闭{/t}</span>{/if}</td>
+                <td>
+                    <a class="change_status" style="cursor: pointer;"  data-msg='{if $list.status eq 'open'}{t domain="mail"}您确定要关闭该推送事件吗？{/t}{else}{t domain="push"}您确定要开启该推送事件吗？{/t}{/if}' data-href='{if $list.status eq "open"}{url path="push/admin_events/close" args="code={$list.code}&id={$list.id}"}{else}{url path="push/admin_events/open" args="code={$list.code}&id={$list.id}"}{/if}' >
+                    {if $list.status eq 'open'}
+                    <button class="btn btn-mini btn-success" type="button">{t domain="push"}点击关闭{/t}</button>
+                    {else}
+                    <button class="btn btn-mini" type="button">{t domain="push"}点击开启{/t}</button>
+                    {/if}
+                    </a>
+                </td>
+            </tr>
+            <!-- {foreachelse} -->
+            <tr><td class="no-records" colspan="5">{t domain="push"}没有找到任何记录{/t}</td></tr>
+            <!-- {/foreach} -->
+            </tbody>
+        </table>
 	</div>
 </div>
 <!-- {/block} -->
